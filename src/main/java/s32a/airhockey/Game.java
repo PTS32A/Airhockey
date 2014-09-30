6890cd4482f39a91172b191d7be2deb08066ae25
Dennis <Dennis@LaptopDennis>
2014-09-30 13:43:45 +0200

    game stubs

--- src/main/java/s32a/airhockey/Game.java
+++ src/main/java/s32a/airhockey/Game.java
@@ -3,14 +3,110 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package s32a.airhockey;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
 
 /**
  *
  * @author Kargathia
  */
-public class Game
-{
-    
+public class Game {
+
+    @Getter private int sideLength;
+    @Getter private List<String> gameInfo;
+    @Getter private boolean isPaused;
+    @Getter private String gameID;
+    @Getter private int roundNo;
+
+    /**
+     *
+     * @param message
+     * @param from
+     */
+    public void addChatMessage(String message, Person from) {
+    }
+
+    /**
+     *
+     * @param starter
+     */
+    public Game(Player starter) {
+
+    }
+
+    /**
+     *
+     * @param player
+     * @return
+     */
+    public boolean addPlayer(Player player) {
+    }
+
+    /**
+     *
+     * @param spectator
+     * @return
+     */
+    public boolean addSpectator(Spectator spectator) {
+    }
+
+    /**
+     *
+     * @param spectator
+     * @return
+     */
+    public boolean removeSpectator(Spectator spectator) {
+    }
+
+    /**
+     *
+     * @return
+     */
+    public boolean beginGame() {
+    }
+
+    /**
+     *
+     * @param puckSpeed
+     * @return
+     */
+    public boolean adjustDifficulty(float puckSpeed) {
+    }
+
+    /**
+     *
+     * @param isPaused
+     * @return
+     */
+    public boolean pauseGame(boolean isPaused) {
+    }
+
+    /**
+     *
+     */
+    public void endGame() {
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Game update() {
+    }
+
+    /**
+     *
+     */
+    private void run() {
+    }
+
+    /**
+     *
+     */
+    private void startRound() {
+    }
+
 }
