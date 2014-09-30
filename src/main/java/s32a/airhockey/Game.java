4c69e335b017e11cef283e783d9f2c7e55d1638c
Dennis <Dennis@LaptopDennis>
2014-09-30 16:48:29 +0200

    game Java docs

--- src/main/java/s32a/airhockey/Game.java
+++ src/main/java/s32a/airhockey/Game.java
@@ -13,113 +13,121 @@ import lombok.Getter;
  *
  * @author Kargathia
  */
-public class Game 
+public class Game
 {
 
-    @Getter private int sideLength;
-    @Getter private List<String> gameInfo;
-    @Getter private boolean isPaused;
-    @Getter private String gameID;
-    @Getter private int roundNo;
-    @Getter private List<Player> myPlayers;
+    @Getter
+    private int sideLength;
+    @Getter
+    private List<String> gameInfo;
+    @Getter
+    private boolean isPaused;
+    @Getter
+    private String gameID;
+    @Getter
+    private int roundNo;
 
     /**
      *
-     * @param message
-     * @param from
+     * @param message The message that is going to be sent to the chat
+     * @param from The player that is sending the message
      */
-    public void addChatMessage(String message, Person from) 
+    public void addChatMessage(String message, Person from)
     {
     }
 
     /**
      *
-     * @param starter
+     * @param starter The player that starts the game initially
      */
-    public Game(Player starter) 
+    public Game(Player starter)
     {
 
     }
 
     /**
      *
-     * @param player
-     * @return
+     * @param player The player that's going to be added to the active game
+     * @return returns true when the player was successfully added
      */
-    public boolean addPlayer(Player player) 
+    public boolean addPlayer(Player player)
     {
     }
 
     /**
      *
-     * @param spectator
-     * @return
+     * @param spectator The spectator that's going to be added to the active
+     * game
+     * @return returns true when the spectator was successfully added.
      */
-    public boolean addSpectator(Spectator spectator) 
+    public boolean addSpectator(Spectator spectator)
     {
     }
 
     /**
      *
-     * @param spectator
-     * @return
+     * @param spectator The spectator that needs to be removed from the active
+     * game
+     * @return returns true if the spectator was successfully removed
      */
-    public boolean removeSpectator(Spectator spectator) 
+    public boolean removeSpectator(Spectator spectator)
     {
     }
 
     /**
      *
-     * @return
+     * @return returns true if the game was started
      */
-    public boolean beginGame() 
+    public boolean beginGame()
     {
     }
 
     /**
      *
-     * @param puckSpeed
-     * @return
+     * @param puckSpeed The speed of the puck
+     * @return returns true if the speed has been successfully adjusted.
      */
-    public boolean adjustDifficulty(float puckSpeed) 
+    public boolean adjustDifficulty(float puckSpeed)
     {
     }
 
     /**
      *
-     * @param isPaused
-     * @return
+     * @param isPaused Set true if the game needs to be paused, false for
+     * un-pausing
+     * @return returns true if the pause change was successful.
      */
-    public boolean pauseGame(boolean isPaused) 
+    public boolean pauseGame(boolean isPaused)
+    {
+    }
+
+    /**
+     * Method for ending the active game
+     */
+    public void endGame()
     {
     }
 
     /**
      *
+     * @return Returns the game in an updated state
      */
-    public void endGame() 
+    public Game update()
     {
     }
 
     /**
-     *
-     * @return
+     *This method cycles to a new frame (puck position, bot position)
+     * ToBeImplemented
      */
-    public Game update() 
+    private void run()
     {
     }
 
     /**
-     *
+     *Starts a new round within the running game
      */
-    private void run() 
-    {
-    }
-
-    /**
-     *
-     */
-    private void startRound() 
+    private void startRound()
     {
     }
 
