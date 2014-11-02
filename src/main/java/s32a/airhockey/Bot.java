8ba0158bafd36d6dd7b01150d0df820d703486ff
bartbouten <bart.bouten@student.fontys.nl>
2014-11-03 00:25:35 +0100

    Bot correction

--- src/main/java/s32a/airhockey/Bot.java
+++ src/main/java/s32a/airhockey/Bot.java
@@ -38,22 +38,22 @@ public class Bot extends Player
         {
             if(this.getColor() == Colors.Red)
             {
-                if(getMyGame().getMyPuck().getPosition().x >= getBatPos().x)
+                if(getMyGame().getMyPuck().getPosition().x >= getBatPos().x - 50)
                 {
                     moveBat(-1);
                 } 
-                if(getMyGame().getMyPuck().getPosition().x <= getBatPos().x)
+                if(getMyGame().getMyPuck().getPosition().x <= getBatPos().x - 50)
                 {
                     moveBat(1);
                 }
             }
             else
             {
-                if(getMyGame().getMyPuck().getPosition().y >= getBatPos().y)
+                if(getMyGame().getMyPuck().getPosition().y >= getBatPos().y - 50)
                 {
                     moveBat(-1);
                 } 
-                if(getMyGame().getMyPuck().getPosition().y <= getBatPos().y)
+                if(getMyGame().getMyPuck().getPosition().y <= getBatPos().y - 50)
                 {
                     moveBat(1);
                 } 
