122521adc2d8c62dbf6bce8f54055c26cb7e20b2
LukeArthurs <luke0519@gmail.com>
2014-12-02 14:22:22 +0100

    LobbyClient

--- src/main/java/s32a/Client/ClientData/LobbyClient.java
+++ src/main/java/s32a/Client/ClientData/LobbyClient.java
@@ -7,16 +7,109 @@ package s32a.Client.ClientData;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import s32a.Shared.IChatbox;
+import s32a.Shared.IGame;
+import s32a.Shared.ILobby;
 import s32a.Shared.ILobbyClient;
+import s32a.Shared.IPerson;
+import s32a.Shared.IPlayer;
 
 /**
  *
  * @author Kargathia
  */
-public class LobbyClient extends UnicastRemoteObject implements ILobbyClient {
+public class LobbyClient extends UnicastRemoteObject implements ILobbyClient, ILobby {
 
     public LobbyClient() throws RemoteException {
 
+    }
+
+    @Override
+    public IPerson getMyPerson(String playerName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public IChatbox getMychatbox() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public HashMap getAirhockeySettings() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public HashMap<String, IPerson> getActivePersons() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<IGame> getActiveGames() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean addPerson(String playerName, String passWord) throws IllegalArgumentException, SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean checkLogin(String playerName, String password) throws IllegalArgumentException, SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean logOut(IPerson input) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public IGame startGame(IPerson person) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public IGame joinGame(IGame game, IPerson person) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public IGame spectateGame(IGame gameInput, IPerson personInput) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean addChatMessage(String message, String from) throws IllegalArgumentException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean endGame(IGame game, IPlayer hasLeft) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void stopSpectating(IGame game, IPerson spectator) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public IGame getMyGame(String gameID) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<IPerson> getRankings() throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void populate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
