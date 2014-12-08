2d4a89d2d9de943c6758f500a2b19a3252446443
LukeArthurs <luke0519@gmail.com>
2014-12-08 14:36:38 +0100

    AirhockeyServer GUI

--- src/main/java/s32a/Server/AirhockeyServer.java
+++ src/main/java/s32a/Server/AirhockeyServer.java
@@ -5,20 +5,36 @@
  */
 package s32a.Server;
 
+import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
+import javafx.application.Application;
 import static javafx.application.Application.launch;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import s32a.Shared.ILobby;
 
 /**
  *
  * @author Kargathia
  */
-public class AirhockeyServer {
+public class AirhockeyServer extends Application {
 
     private Lobby lobby;
+    private Stage stage;
     private static final int portNumber = 1099;
     private static final String bindingName = "AirhockeyServer";
 
@@ -52,6 +68,56 @@ public class AirhockeyServer {
         }
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.stage = primaryStage;
+        GridPane gp = new GridPane();
+        gp.setAlignment(Pos.CENTER);
+        gp.setHgap(10);
+        gp.setVgap(10);
+        gp.setPadding(new Insets(25, 25, 25, 25));
+        Label ip = new Label("Ip Address:");
+        gp.add(ip, 0, 0);
+        Label ipIn = new Label(InetAddress.getLocalHost().toString());
+        gp.add(ipIn, 1, 0);
+        Label port = new Label("Port:");
+        gp.add(port, 0, 2);
+        Label portIn = new Label(String.valueOf(portNumber));
+        gp.add(portIn, 1, 2);
+        Label games = new Label("Active Games:");
+        gp.add(games, 0, 3);
+        Label gamesIn = new Label("0");
+        gp.add(gamesIn, 1, 3);
+        Label person = new Label("Active Users:");
+        gp.add(person, 0, 4);
+        Label personIn = new Label("0");
+        gp.add(personIn, 1, 4);
+        
+        // Binding 
+        //gamesIn.textProperty().bind(lobby.getActiveGames().size());
+        //personIn.textProperty().bind(lobby.getActivePersons().size());
+        
+//        btn.setOnAction(new EventHandler<ActionEvent>() {
+//
+//            @Override
+//            public void handle(ActionEvent e) {
+//                 
+//            }
+//        });
+        Group root = new Group();
+        Scene scene = new Scene(root, 300, 300);
+        root.getChildren().add(gp);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Server Information");
+        primaryStage.show();
+        
+        primaryStage.setOnCloseRequest((WindowEvent event) ->
+        {
+            Platform.exit();
+            System.exit(0);
+        });
+    }
+    
     /**
      * Runs the program as server - for debugging only. In release running as
      * server is handled by running AirhockeyGUI with "server" as argument
@@ -59,7 +125,7 @@ public class AirhockeyServer {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        AirhockeyServer server = new AirhockeyServer();
+        //AirhockeyServer server = new AirhockeyServer();
+        launch(args);
     }
-
 }
