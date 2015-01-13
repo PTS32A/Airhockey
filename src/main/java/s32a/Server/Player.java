b374015abca48d92b6676294c7c1b44412fc308a
Juliusername <juliusodb@gmail.com>
2015-01-13 12:40:05 +0100

    Changed: bats can now move during countdown

--- src/main/java/s32a/Server/Player.java
+++ src/main/java/s32a/Server/Player.java
@@ -113,7 +113,9 @@ public class Player extends Person implements IPlayer {
         double cX = sideLength / 2;
         double cY = 0;
 
-        if (!((Game) myGame).statusProperty().get().equals(GameStatus.Playing)) {
+        if (!((Game) myGame).statusProperty().get().equals(GameStatus.Playing) 
+                && !((Game) myGame).statusProperty().get().equals(GameStatus
+                        .Waiting)) {
             return false;
         } else {
             // Will reimplement this later.
