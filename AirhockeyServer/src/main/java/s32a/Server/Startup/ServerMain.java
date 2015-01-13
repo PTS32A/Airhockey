9347cf85c78a8acaee6f4c11ddb5cc670b7683d0
LukeArthurs <luke0519@gmail.com>
2015-01-13 15:54:56 +0100

    ServerSetUpGui's Done

--- AirhockeyServer/src/main/java/s32a/Server/Startup/ServerMain.java
+++ AirhockeyServer/src/main/java/s32a/Server/Startup/ServerMain.java
@@ -11,10 +11,13 @@ import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -37,6 +40,7 @@ public class ServerMain extends Application{
 
     
     private Stage stage;
+    private ServerInfo serverInfo;
 
     @Override
     public void start(Stage primaryStage) {
@@ -53,17 +57,17 @@ public class ServerMain extends Application{
                     gp.setHgap(10);
                     gp.setVgap(10);
                     gp.setPadding(new Insets(25, 25, 25, 25));
-                    Label ip = new Label(":");
+                    Label ip = new Label("FTP website:");
                     gp.add(ip, 0, 1);
                     TextField tfIp = new TextField();
                     tfIp.setText("athena.fhict.nl");
                     gp.add(tfIp, 1, 1);
-                    Label port = new Label("Port:");
+                    Label port = new Label("Status:");
                     gp.add(port, 0, 2);
                     TextField tfPort = new TextField();
-                    tfPort.setText("1099");
+                    tfPort.setText("Not Connected");
                     gp.add(tfPort, 1, 2);
-                    Button confirm = new Button("Confirm");
+                    Button confirm = new Button("Connect");
                     gp.add(confirm, 1, 3);
                     confirm.setOnAction(new EventHandler<ActionEvent>() {
 
@@ -73,6 +77,14 @@ public class ServerMain extends Application{
                             try {
                                 client.connect(tfIp.getText());
                                 boolean login = client.login("i293443", "ifvr2edfh101");
+                                if (login) {
+                                    client.logout();
+                                    stage.close();
+                                    serverSetUp();
+                                }
+                                else{
+                                    tfPort.setText("Could not connect, check spelling and internet connection.");
+                                }
                             } catch (IOException ex) {
                                 ex.printStackTrace();
                             }
@@ -100,19 +112,109 @@ public class ServerMain extends Application{
                 }
                 catch (Exception ex) {
                     //showDialog("Error", "Could not open game: " + ex.getMessage());
+                    System.out.println(ex.toString());
                 }
             }
        });
-       while (stage.isShowing()) {
-
-       }
     }
     
+    private void serverSetUp() throws IOException
+    {
+        Platform.runLater(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    Stage stage = new Stage();
+                    GridPane gp = new GridPane();
+                    gp.setAlignment(Pos.CENTER);
+                    gp.setHgap(10);
+                    gp.setVgap(10);
+                    gp.setPadding(new Insets(25, 25, 25, 25));
+                    Label serverName = new Label("Server name:");
+                    gp.add(serverName, 0, 1);
+                    TextField tfSN = new TextField();
+                    tfSN.setText("Airhockey");
+                    gp.add(tfSN, 1, 1);
+                    Label bind = new Label("Binding Name:");
+                    gp.add(bind, 0, 2);
+                    TextField tfBind = new TextField();
+                    tfBind.setText("AirhockeyServer");
+                    gp.add(tfBind, 1, 2);
+                    Label ip = new Label("IP Adress:");
+                    gp.add(ip, 0, 3);
+                    TextField tfIP = new TextField();
+                    tfIP.setText("0.0.0.0");
+                    gp.add(tfIP, 1, 3);
+                    Label port = new Label("Port:");
+                    gp.add(port, 0, 4);
+                    TextField tfPort = new TextField();
+                    tfPort.setText("1099");
+                    gp.add(tfPort, 1, 4);
+                    Label desc = new Label("Description");
+                    gp.add(desc, 0, 5);
+                    TextField tfDesc = new TextField();
+                    tfDesc.setText("");
+                    gp.add(tfDesc, 1, 5);
+                    Button confirm = new Button("Start Server");
+                    gp.add(confirm, 1, 6);
+                    confirm.setOnAction(new EventHandler<ActionEvent>() {
+
+                        @Override
+                        public void handle(ActionEvent e) {
+                            if(serverInfo(tfSN.getText(), tfBind.getText(),
+                                    tfIP.getText(), tfPort.getText(), tfDesc.getText())){
+                                //Todo
+                                stage.close();
+                            }
+                            else{
+                                //Error
+                            }
+                        }
+                    });
+
+                    Group root = new Group();
+                    Scene scene = new Scene(root, 300, 300);
+                    root.getChildren().add(gp);
+                    stage.setScene(scene);
+                    stage.setTitle("Start New Server");
+
+                    stage.getScene().setOnKeyPressed(new EventHandler<KeyEvent>() {
+
+                        @Override
+                        public void handle(KeyEvent ke) {
+                            if (ke.getCode() == KeyCode.ENTER) {
+                                confirm.fire();
+                            }
+                        }
+                    });
+
+                    stage.show();
+                }
+                catch (Exception ex) {
+                    //showDialog("Error", "Could not open game: " + ex.getMessage());
+                    System.out.println(ex.toString());
+                }
+            }
+       });
+    }
+    
+    public boolean serverInfo(String name, String bind, String ip, String port, String description)
+    {
+        try{
+            this.serverInfo = new ServerInfo(name, description, bind, ip, Integer.valueOf(port));
+            return true;
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return false;
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
     }
 
     
