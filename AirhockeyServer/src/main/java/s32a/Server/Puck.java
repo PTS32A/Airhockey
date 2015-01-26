bd6bd821fc5d1ac0a5c49d70ae0405fd9147c0d0
LukeArthurs <luke0519@gmail.com>
2015-01-26 12:28:40 +0100

    Puck blue/green fixed

--- AirhockeyServer/src/main/java/s32a/Server/Puck.java
+++ AirhockeyServer/src/main/java/s32a/Server/Puck.java
@@ -556,18 +556,18 @@ public class Puck extends TimerTask {
                 
                 if (p.getColor() == Colors.Green) {
                     direction = 120;
-                    result.y = (float)p.getPosY().get() + (int)Math.round(radius * Math.sin(Math.toRadians(330)));
-                    result.x = (float)p.getPosX().get() + (int)Math.round(radius * Math.cos(Math.toRadians(330)));
-                    batBouncePosition = new Vector2((float) (p.getPosX().get() - radius), (float) (p.getPosY().get() - puckSize / 2));
-                    xPosBat.bind(Bindings.subtract(p.getPosX(), radius));
-                    yPosBat.bind(Bindings.subtract(p.getPosY(), puckSize / 2));
+//                    result.y = (float)p.getPosY().get() + (int)Math.round(radius * Math.sin(Math.toRadians(330)));
+//                    result.x = (float)p.getPosX().get() + (int)Math.round(radius * Math.cos(Math.toRadians(330)));
+                    batBouncePosition = new Vector2(result.x, result.y);
+                    xPosBat.bind(Bindings.add(p.getPosX(), (int)Math.round(radius * Math.sin(Math.toRadians(330)))));
+                    yPosBat.bind(Bindings.add(p.getPosY(), (int)Math.round(radius * Math.cos(Math.toRadians(330)))));
                 } else if (p.getColor() == Colors.Blue) {
                     direction = 60;
-                    result.y = (float)p.getPosY().get() + (int)Math.round(radius * Math.sin(Math.toRadians(210)));
-                    result.x = (float)p.getPosX().get() + (int)Math.round(radius * Math.cos(Math.toRadians(210)));
-                    batBouncePosition = new Vector2((float) (p.getPosX().get() + radius), (float) (p.getPosY().get() - puckSize / 2));
-                    xPosBat.bind(Bindings.add(p.getPosX(), radius));
-                    yPosBat.bind(Bindings.subtract(p.getPosY(), puckSize / 2));
+//                    result.y = (float)p.getPosY().get() + (int)Math.round(radius * Math.sin(Math.toRadians(210)));
+//                    result.x = (float)p.getPosX().get() + (int)Math.round(radius * Math.cos(Math.toRadians(210)));
+                    batBouncePosition = new Vector2(result.x, result.y);
+                    xPosBat.bind(Bindings.add(p.getPosX(), (int)Math.round(radius * Math.sin(Math.toRadians(210)))));
+                    yPosBat.bind(Bindings.add(p.getPosY(), (int)Math.round(radius * Math.cos(Math.toRadians(210)))));
                 } else {
                     direction = 0;
                     batBouncePosition = new Vector2((float) (p.getPosX().get()), (float) (p.getPosY().get() + radius));
